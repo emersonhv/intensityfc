1fc2cc02cc1e38dc8f588d805ce2bb3e9e86d5a1
kalfaroperez <kalfaroperez@gmail.com>
2016-07-09 22:28:48 -0500

    Se cambio vista de planes

--- application/views/admin/ver_planes.php
+++ application/views/admin/ver_planes.php
@@ -22,22 +22,45 @@
 		color:white;
 	}
 </style>
+<a type="button" class="btn btn-primary btn-flat" href="cliente">
+    <spam>Nuevo Plan</spam> <i class="fa fa-pencil"></i>
+</a>
 <div class="row" ng-controller="VerPlanes" ng-init="indx = 1">
-    <!-- /.col -->
-    <div class="col-md-6 col-sm-6 col-xs-12" ng-repeat="plan in planes">
-        <!-- PLANES -->
-        <div class="small-box {{color[$index]}}">
-            <div class="inner" style="text-align: center;">
-                <h3 style="text-align: center;">{{plan.name}}</h3>
-                <!--i class="fa fa-star fa-style" ng-repeat="plan in planes | limitTo:$index+1"></i-->
-            </div>
-            <div class="" style="text-align: center;">
-                <p>{{plan.description.split('\n')[0]}}</p>
-                <p>{{plan.description.split('\n')[1]}}</p>
-            </div>
-            <a href="#" class="small-box-footer" style="font-size: 20px;">{{plan.price[0].price | currency}} COP</a>
+<div class="input-group margin">
+                <input type="text" class="form-control" ng-model="nombrecl" placeholder="Ingrese nombre de cliente">
+            <span class="input-group-btn">
+                <button type="button" class="btn btn-info btn-flat" ng-click="buscarCliente(nombrecl)">Buscar</button>
+            </span>
+            <!--div class="input-group-btn">
+                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+            </div-->
         </div>
-    </div>
-    
+        
+<table class="table table-bordered table-hover">
+    <tbody>
+    <tr>
+        <th>
+            <i class="fa fa-check-square-o"></i>
+        </th>
+        <th>Nombre</th>
+        <th>Referencia</th>
+        <th>Cantidad Citas</th>
+        <th>Color</th>
+    </tr>
+    <tr ng-repeat="plan in planes | filter:nombrecl">
+        <td>
+            <!--input class="" type="radio" id="cliente{{$index+1}}" name="cliente" value="{{cliente.id}}" ng-click="seleccionarClie(this)"-->
+            <!--a id="cliente{{$index+1}}" name="cliente" value="{{cliente.id}}" class="btn btn-info btn-flat" href="prueba.intensityfc.com/citas_cliente/:id">Ver Citas</a-->
+            <a id="plan{{$index+1}}" name="plan" value="{{plan.id}}" class="label btn-info" href="plan/{{ plan.id }}">Ver Plan</a>
+            <a id="plan{{$index+1}}" name="plan" value="{{plan.id}}" class="label btn-info" href="plan/{{ plan.id }}">Editar</a>
+        </td>
+        <td><a>{{plan.name}}</a></td>
+        <td>{{plan.description.split('\n')[0]}}</td>
+        <td>{{plan.price[0].price | currency}} COP</td>
+    </tr>
+    </tbody>
+</table>
+
+
     <!-- /.col -->
 </div>
