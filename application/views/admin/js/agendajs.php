3cd9da724519147cdd15bb506c63e0288bc21995
Emerson <emersonhv@gmail.com>
2016-07-17 08:25:07 -0500

    >>> optener ultimas cambios

--- application/views/admin/js/agendajs.php
+++ application/views/admin/js/agendajs.php
@@ -22,7 +22,7 @@
 				 if(cit[i].nombre_plan.indexOf("CLASE CORTESIA") > -1){
 					 backgroundColor = "#3c8dbc";//azul
 					 borderColor = "#3c8dbc";//azul
-				 }else{
+				 } else {
 					 backgroundColor = "#00a65a";//green
 					 borderColor = "#00a65a";//green
 				 }
@@ -38,7 +38,7 @@
            console.log($scope.eventos);
            calendar($scope.eventos);
        }, function(response) {
-         alert("Hubo un problema al traer los datos del servidor, recargue la página si persiste contacte con el administrador del sistema.");
+           alert("Hubo un problema al traer los datos del servidor, recargue la página si persiste contacte con el administrador del sistema.");
            $scope.mensaje = {
              msg:"No se envió información, comuniquese con el proveedor",
              tipo:"alert-warning"
