f0d9ca499773fb948122837e75bb6de32391004c
kalfaroperez <kalfaroperez@gmail.com>
2016-07-20 07:34:53 -0500

    ...

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -115,7 +115,6 @@ class Admin extends MY_Controller {
 		$paramts['mensaje'] = $mensaje;
 		$paramts['desc_titulo'] = 'Agenda de citas';
 		$paramts['javascript'] = $this->load->view('admin/js/agendajs','', TRUE);
-		$paramts['javascript'] = $this->load->view('admin/js/cliente_listajs','', TRUE);
 		$this->load->view('layout/master',$paramts);
 	}
 
@@ -128,6 +127,7 @@ class Admin extends MY_Controller {
 		$paramts['mensaje'] = $mensaje;
 		$paramts['desc_titulo'] = 'Agenda de citas';
 		$paramts['javascript'] = $this->load->view('admin/js/agendajs','', TRUE);
+		$paramts['javascript'] = $this->load->view('admin/js/cliente_listajs','', TRUE);
 		$this->load->view('layout/master',$paramts);
 	}
 
