ddf83266910e42eda1f329ebbcd086c6fa2ee219
kalfaroperez <kalfaroperez@gmail.com>
2016-07-20 18:20:49 -0500

    ---

--- application/views/admin/nueva_agenda.php
+++ application/views/admin/nueva_agenda.php
@@ -26,8 +26,8 @@
                     <div class="modal-body">
                         <!-- Drop down Clientes -->
                         <label>Cliente</label>
-                        <select class="form-control select2" id="cbxClientes" style="width: 100%;" ng-model="clientes">
-                            <option value="{{cliente.id}}" ng-repeat="cliente in clientes">{{ cliente.name }}</option>
+                        <select class="form-control select2" id="cbxClientes" style="width: 100%;" ng-model="cliente">
+                            <option ng-repeat="option in clientes" value="{{option.id}}" >{{ option.name }}</option>
                         </select>
 
                         <!-- /Drop down Clientes -->
