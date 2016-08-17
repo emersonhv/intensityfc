6f5c85142a017bed99a49db04e1c50927ac78226
kalfaroperez <kalfaroperez@gmail.com>
2016-08-17 05:40:26 -0500

    ._.

--- application/config/database.php
+++ application/config/database.php
@@ -53,10 +53,12 @@ $db['default']['username'] = 'u301668190_test';
 $db['default']['password'] = 'rk8b09lMad';
 $db['default']['database'] = 'u301668190_test';
 
-/*$db['default']['hostname'] = 'localhost';
+/*
+$db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = '';
 $db['default']['database'] = 'u301668190_test';*/
+
 $db['default']['dbdriver'] = 'mysql';$db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = FALSE;
